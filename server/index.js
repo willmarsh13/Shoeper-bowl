dbf4eff920c529d58bb4ff13a5f4bfd4a37fe109
Will Marsh <70150219+willmarsh13@users.noreply.github.com>
2026-01-04 19:55:25 -0500

    Update index.js

--- server/index.js
+++ server/index.js
@@ -23,6 +23,11 @@ app.set("trust proxy", true);
 app.use(bodyParser.json({limit: '50mb'}));
 app.use(bodyParser.urlencoded({limit: '50mb', extended: true, parameterLimit: 500000}));
 
+app.use(
+    "/shoeper-bowl",
+    express.static(path.resolve(__dirname, "../client/build"))
+);
+
 /**
  * Unprotected PATHs
  */
